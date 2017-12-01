40c7cf89b739a53b840fd9287a8f2b134fbe8434
eriksteigert <erik.steigert@web.de>
2017-12-01 15:13:50 +0100

    hallo

--- src/qualitaetssicherung/BackToBackTest.java
+++ src/qualitaetssicherung/BackToBackTest.java
@@ -1,9 +1,11 @@
 package qualitaetssicherung;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 public class BackToBackTest {
 	
@@ -19,11 +21,20 @@ public class BackToBackTest {
 		BufferedReader br =  new BufferedReader(new FileReader("src/qualitaetssicherung/testdata.txt"));
 		PrintWriter    pw1 = new PrintWriter("src/qualitaetssicherung/output1.txt");
 		PrintWriter    pw2 = new PrintWriter("src/qualitaetssicherung/output2.txt");
+		String testData;
+		int firstNumber;
+		int secondNumber;
 		
-		pw1.println(br.readLine());
-		pw2.println("Testprint2");
+		generateTestDatas();
 		
 		//Softwareversionen mit Testdaten ausführen und Ergebnisse speichern.
+		while ((testData = br.readLine()) != null && testData.length() != 0) {
+			String[] parts = testData.split(" ");
+			firstNumber = Integer.parseInt(parts[0]);
+			secondNumber = Integer.parseInt(parts[1]);
+			pw1.println(swv1.isSquareRootOf(firstNumber, secondNumber));
+			pw2.println(swv2.isSquareRootOf(firstNumber, secondNumber));
+		}
 		
 		
 		//Ergebnisse der Softwareversionen miteinander vergleichen
@@ -32,6 +43,33 @@ public class BackToBackTest {
 		br.close();
 		pw1.close();
 		pw2.close();
+	}
+	
+	private void generateTestDatas() throws FileNotFoundException {		
+		PrintWriter pw = new PrintWriter("src/qualitaetssicherung/testdata.txt");
+		
+		pw.println("5 -25");
+		pw.println("-5 25");
+		pw.println("-5 -25");
+		
+		//Korrekte Testdaten
+		for(int i = 0; i < 20; i++) {
+			Random rand = new Random();
+			int minRange = 1;
+			int maxRange = 1000;
+			int value = rand.nextInt((maxRange - minRange) + 1) + minRange;
+			pw.println(value + " " + (value * value));
+		}
+		
+		//Zufällige Testdaten
+		for(int i = 0; i < 20; i++) {
+			Random rand = new Random();
+			int value1 = rand.nextInt();
+			int value2 = rand.nextInt();
+			pw.println(value1 + " " + value2);
+		}
+		
+		pw.close();
 	}
 	
 }
--- src/qualitaetssicherung/Softwareversion1.java
+++ src/qualitaetssicherung/Softwareversion1.java
@@ -2,4 +2,10 @@ package qualitaetssicherung;
 
 public class Softwareversion1 {
 
+	public boolean isSquareRootOf(int number1, int number2) {
+		if(number1 < 1 || number2 < 1) return false;
+		int square = (int)Math.sqrt(number2);		
+		return square == number1;
+	}
+
 }
